9fef912ec033699fbdf32dcfd0b21b7ec30d24c5
Ingo Ruhnke <grumbel@gmail.com>
2000-05-23 21:08:43 +0000

    Added a trivial timing class

---
+++ src/Timer.hh
@@ -0,0 +1,36 @@
+//  $Id: Timer.hh,v 1.1 2000/05/23 21:08:43 grumbel Exp $
+// 
+//  Pingus - A free Lemmings clone
+//  Copyright (C) 2000 Ingo Ruhnke <grumbel@gmx.de>
+//
+//  This program is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU General Public License
+//  as published by the Free Software Foundation; either version 2
+//  of the License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+// 
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+#ifndef TIMER_HH
+#define TIMER_HH
+
+class Timer
+{
+private:
+  int last_time;
+  bool running;
+public:
+  Timer();
+  int start();
+  int stop();
+};
+
+#endif
+
+/* EOF */
