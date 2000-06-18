3296181a8f4a2a13948718f4a01060e25fa9a57f
Ingo Ruhnke <grumbel@gmail.com>
2000-06-18 18:14:47 +0000

    Misc changes

--- src/blitter.hh
+++ src/blitter.hh
@@ -1,4 +1,4 @@
-//  $Id: blitter.hh,v 1.6 2000/06/18 17:01:50 grumbel Exp $
+//  $Id: blitter.hh,v 1.7 2000/06/18 18:14:47 grumbel Exp $
 //
 //  Pingus - A free Lemmings clone
 //  Copyright (C) 1999 Ingo Ruhnke <grumbel@gmx.de>
@@ -22,7 +22,9 @@
 
 #include <ClanLib/core.h>
 
-///
+/** A bunch of blitting and creation functions to operate on
+    CL_Canvas.  Some of them a similar to the ones in ClanLib, but
+    this are slower and work. */
 class Blitter
 {
 private:
@@ -33,32 +35,38 @@ private:
   static void put_surface_32bit(CL_Canvas*, CL_SurfaceProvider*,
 			       int x, int y);
 public:
-  /**void put_surface(CL_LockableSurface* provider, CL_Surface* surface,
+  /*void put_surface(CL_LockableSurface* provider, CL_Surface* surface,
     int x, int y);
     void put_surface(CL_LockableSurface* provider, CL_SurfaceProvider* surface,
     int x, int y);
   */
+
+  /// Puts a given surface to a given canvas at position x, y.
   static void put_surface(CL_Canvas*, CL_Surface*,
 			  int x, int y);
 
-  ///
+  /// Puts a given surface provider to a given canvas at position x, y.
   static void put_surface(CL_Canvas*, CL_SurfaceProvider*,
 			  int x, int y);
 
-  ///
-  static void put_alpha_surface(CL_Canvas*, CL_SurfaceProvider*,
+  /** Makes all pixels in canvas tranparent, when their indexed value
+      in provider is larger than zero.*/
+  static void put_alpha_surface(CL_Canvas* canvas, CL_SurfaceProvider* provider,
 				int x, int y);
 
-  ///
+  /** Returns a newly allocated canvas. The canvas contains the same
+      image as the given surface. */
   static CL_Canvas* create_canvas(CL_Surface*);
-  ///
+
+  /** Returns a newly allocated canvas. The canvas contains the same
+      image as the given surface provider */
   static CL_Canvas* create_canvas(CL_SurfaceProvider*);
-  ///
+
+  /** Sets all pixels of a canvas to zero */
   static CL_Canvas* clear_canvas(CL_Canvas*);
   
   //static CL_Surface* convert_to_emptyprovider(CL_Surface*);
-  ///static CL_Canvas* convert_to_emptyprovider(CL_SurfaceProvider*);
-  
+  //static CL_Canvas* convert_to_emptyprovider(CL_SurfaceProvider*);
 };
 
 #endif 
