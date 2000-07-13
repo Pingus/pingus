3ce8450474443ff5fe896bbb47f0a14adc801c7a
Ingo Ruhnke <grumbel@gmail.com>
2000-07-13 20:05:27 +0000

    A basic plf2xml converter, next step is to build a xml parser into pingus

---
+++ src/plf2xml/plf2xml.hh
@@ -0,0 +1,26 @@
+//  $Id: plf2xml.hh,v 1.1 2000/07/13 20:05:27 grumbel Exp $
+// 
+//  Pingus - A free Lemmings clone
+//  Copyright (C) 2000 Ingo Ruhnke <grumbel@gmx.de>
+//
+//  This program is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU General Public License
+//  as published by the Free Software Foundation; either version 2
+//  of the License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+// 
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+#ifndef PLF2XML_HH
+#define PLF2XML_HH
+
+
+#endif
+
+/* EOF */
