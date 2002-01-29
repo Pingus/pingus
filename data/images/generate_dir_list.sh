2c1705687a2fc0c7ceb8592fed75a237109604db
Ingo Ruhnke <grumbel@gmail.com>
2002-01-29 20:38:39 +0000

    why, oh why, have I this feeling that I just reinvented the wheel...

--- data/images/generate_dir_list.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-DIRECTORY=`echo $1 | sed -e "s/\//\\\\\\\\\//g"` # <- $!&@^%^&*!@...
-sh ./generate_images_list.sh | sed "s/^\(.*\)\/.*$/$DIRECTORY\/\1/" | sed "s/^\/\(.*\)\$/\1\//" | sort | uniq | grep "."
-
-# EOF #
--- data/images/generate_images_list.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-cat ../data/*.scr | sed -n "s/^[ 	]*[-A-Za-z0-9_]*[ 	]*=[ 	]*images\/\([^ ]*\) *(.*);/\1/p"
-echo 
-
