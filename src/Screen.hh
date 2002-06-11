2314ba47a0a3e09c45dbc89dc925ae712a75b31d
Ingo Ruhnke <grumbel@gmail.com>
2002-06-11 13:24:52 +0000

    some event handling ideas

---
+++ src/Screen.hh
@@ -0,0 +1,45 @@
+//  $Id: Screen.hh,v 1.1 2002/06/11 13:24:52 grumbel Exp $
+// 
+//  Pingus - A free Lemmings clone
+//  Copyright (C) 2000 Ingo Ruhnke <grumbel@gmx.de>
+//
+//  This program is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU General Public License
+//  as published by the Free Software Foundation; either version 2
+//  of the License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+// 
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+#ifndef SCREEN_HH
+#define SCREEN_HH
+
+#include "GameDelta.hh"
+
+namespace Pingus
+{
+  /** A interface for screens. A screen is a Pingus 'thing' which gets
+      complete controll over the display and input. Examples of
+      screens are the PingusMenu or a PingusGameSession */
+  class Screen
+  {
+  private:
+    
+  public:
+    /** Draw this screen */
+    void draw () =0;
+
+    /** Pass a delta to the screen */
+    void update (const GameDelta& delta) =0;
+  };
+}
+
+#endif
+
+/* EOF */
