00a5093f63bb469ad6b8dae52796f62690290e43
Ingo Ruhnke <grumbel@gmail.com>
2002-06-20 11:11:28 +0000

    added missing header

--- src/editor/editor_event.cxx
+++ src/editor/editor_event.cxx
@@ -1,4 +1,4 @@
-//  $Id: editor_event.cxx,v 1.2 2002/06/13 14:25:13 torangan Exp $
+//  $Id: editor_event.cxx,v 1.3 2002/06/20 11:10:17 grumbel Exp $
 //
 //  Pingus - A free Lemmings clone
 //  Copyright (C) 2000 Ingo Ruhnke <grumbel@gmx.de>
@@ -16,6 +16,8 @@
 //  You should have received a copy of the GNU General Public License
 //  along with this program; if not, write to the Free Software
 //  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+#include <stdio.h>
 
 #include <ClanLib/Display/Input/input.h>
 #include <ClanLib/Display/Input/keyboard.h>
