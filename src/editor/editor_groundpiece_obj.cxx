d8111086dff61289bf2fe1e112bc83eaa97864bd
Ingo Ruhnke <grumbel@gmail.com>
2002-06-20 11:12:12 +0000

    added missing header, again...

--- src/editor/editor_groundpiece_obj.cxx
+++ src/editor/editor_groundpiece_obj.cxx
@@ -1,4 +1,4 @@
-//  $Id: editor_groundpiece_obj.cxx,v 1.2 2002/06/13 14:25:13 torangan Exp $
+//  $Id: editor_groundpiece_obj.cxx,v 1.3 2002/06/20 11:12:12 grumbel Exp $
 //
 //  Pingus - A free Lemmings clone
 //  Copyright (C) 1999 Ingo Ruhnke <grumbel@gmx.de>
@@ -17,8 +17,8 @@
 //  along with this program; if not, write to the Free Software
 //  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
+#include <stdio.h>
 #include "../boost/smart_ptr.hpp"
-
 #include "editor_groundpiece_obj.hxx"
 
 EditorGroundpieceObj::EditorGroundpieceObj(const GroundpieceData& data)
