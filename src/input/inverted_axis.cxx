8966ecd22ca9300978aa900661416e0590183dac
David Philippi <david@torangan.de>
2002-07-05 09:59:21 +0000

    helper class to invert the grade of another axis

---
+++ src/input/inverted_axis.cxx
@@ -0,0 +1,46 @@
+//  $Id: inverted_axis.cxx,v 1.1 2002/07/05 09:59:20 torangan Exp $
+// 
+//  Pingus - A free Lemmings clone
+//  Copyright (C) 2000 Ingo Ruhnke <grumbel@gmx.de>
+//
+//  This program is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU General Public License
+//  as published by the Free Software Foundation; either version 2
+//  of the License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+// 
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+#include "inverted_axis.hxx"
+
+namespace Input
+{
+  InvertedAxis::InvertedAxis(Axis* axis_) : axis(axis_) { }
+
+
+  float
+  InvertedAxis::get_pos ()
+  {
+    return axis->get_pos();
+  }
+
+  float
+  InvertedAxis::get_angle ()
+  {
+    return (static_cast<int>(axis->get_angle()) + 180) % 360;
+  }
+  
+  void
+  InvertedAxis::update(float delta)
+  {
+    axis->update(delta);
+  }
+}
+
+/* EOF */
