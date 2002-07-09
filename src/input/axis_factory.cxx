b529d623d03b028b4a614b018877ce51e0e3f799
David Philippi <david@torangan.de>
2002-07-09 16:03:32 +0000

    creates an axis object

---
+++ src/input/axis_factory.cxx
@@ -0,0 +1,106 @@
+//  $Id: axis_factory.cxx,v 1.1 2002/07/09 16:03:32 torangan Exp $
+// 
+//  Pingus - A free Lemmings clone
+//  Copyright (C) 2000 Ingo Ruhnke <grumbel@gmx.de>
+//
+//  This program is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU General Public License
+//  as published by the Free Software Foundation; either version 2
+//  of the License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+// 
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+#include <stdlib.h>
+#include "../xml_helper.hxx"
+#include "../pingus_error.hxx"
+#include "button_axis.hxx"
+#include "inverted_axis.hxx"
+#include "joystick_axis.hxx"
+#include "mouse_axis.hxx"
+#include "axis_factory.hxx"
+#include "button_factory.hxx"
+#include "button.hxx"
+
+namespace Input {
+
+  Axis* AxisFactory::create(xmlNodePtr cur)
+  {
+    if (xmlIsBlankNode(cur)) 
+      cur = cur->next;
+
+    if ( ! strcmp(reinterpret_cast<const char*>(cur->name), "button-axis"))
+      return button_axis(cur);
+      
+    else if ( ! strcmp(reinterpret_cast<const char*>(cur->name), "inverted-axis"))
+      return inverted_axis(cur);
+      
+    else if ( ! strcmp(reinterpret_cast<const char*>(cur->name), "joystick-axis"))
+      return joystick_axis(cur);
+      
+    else if ( ! strcmp(reinterpret_cast<const char*>(cur->name), "mouse-axis"))
+      return mouse_axis(cur);
+
+    else
+      throw PingusError("Unknown axis type: " + std::string(reinterpret_cast<const char*>(cur->name)));
+  }
+  
+  Axis* AxisFactory::button_axis (xmlNodePtr cur)
+  {
+    char * angle_str = reinterpret_cast<char *>(xmlGetProp(cur, reinterpret_cast<const xmlChar*>("id")));
+    float angle = atof(angle_str);
+    free(angle_str);
+
+    cur = cur->children;
+    Button* button1 = ButtonFactory::create(cur);
+    cur = cur->next;
+    Button* button2 = ButtonFactory::create(cur);    
+	    
+    return new ButtonAxis(angle, button1, button2);
+  }
+
+  Axis* AxisFactory::inverted_axis (xmlNodePtr cur)
+  {
+    return new InvertedAxis(create(cur->children));
+  }
+      
+  Axis* AxisFactory::joystick_axis (xmlNodePtr cur)
+  {
+    char * angle_str = reinterpret_cast<char *>(xmlGetProp(cur, reinterpret_cast<const xmlChar*>("angle")));
+    char * id_str    = reinterpret_cast<char *>(xmlGetProp(cur, reinterpret_cast<const xmlChar*>("id")));
+    char * axis_str  = reinterpret_cast<char *>(xmlGetProp(cur, reinterpret_cast<const xmlChar*>("axis")));
+    
+    float angle = atof(angle_str);
+    int   id    = atoi(id_str);
+    int   axis  = atoi(axis_str);
+    
+    free(angle_str);
+    free(id_str);
+    free(axis_str);
+    
+    return new JoystickAxis(id, axis, angle);
+  }
+  
+  Axis* AxisFactory::mouse_axis (xmlNodePtr cur)
+  {
+    char * angle_str = reinterpret_cast<char *>(xmlGetProp(cur, reinterpret_cast<const xmlChar*>("angle")));
+    char * axis_str  = reinterpret_cast<char *>(xmlGetProp(cur, reinterpret_cast<const xmlChar*>("axis")));
+    
+    float angle = atof(angle_str);
+    int   axis  = atoi(axis_str);
+    
+    free(angle_str);
+    free(axis_str);
+    
+    return new MouseAxis(axis, angle);
+  }
+
+}
+
+/* EOF */
