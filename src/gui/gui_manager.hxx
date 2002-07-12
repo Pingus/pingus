e68ecab415b22ec1bf79c0733b2a8c91ec83241a
Ingo Ruhnke <grumbel@gmail.com>
2002-07-12 15:39:21 +0000

    -added scatch of a gui framework

---
+++ src/gui/gui_manager.hxx
@@ -0,0 +1,44 @@
+//  $Id: gui_manager.hxx,v 1.1 2002/07/12 15:39:21 grumbel Exp $
+// 
+//  Pingus - A free Lemmings clone
+//  Copyright (C) 2000 Ingo Ruhnke <grumbel@gmx.de>
+//
+//  This program is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU General Public License
+//  as published by the Free Software Foundation; either version 2
+//  of the License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+// 
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+#ifndef HEADER_PINGUS_GUI_GUI_MANAGER_HXX
+#define HEADER_PINGUS_GUI_GUI_MANAGER_HXX
+
+namespace GUI
+{
+  /** Manager class which holds all GUI components and displays them */
+  class GUIManager
+  {
+  private:
+    std::vector<Component*> components;
+
+  public:
+    GUIManager ();
+    ~GUIManager ();
+    
+    void update (float delta);
+
+    void add (Component*);
+    void remove (Component*);
+  };
+}
+
+#endif
+
+/* EOF */
