b023132c9a13115e3b09c1bf331de372baef8ea1
David Philippi <david@torangan.de>
2002-10-22 19:31:11 +0000

    removed double declaration of move_with_forces

--- src/pingu_action.hxx
+++ src/pingu_action.hxx
@@ -1,4 +1,4 @@
-//  $Id: pingu_action.hxx,v 1.21 2002/10/22 00:21:02 grumbel Exp $
+//  $Id: pingu_action.hxx,v 1.22 2002/10/22 19:31:11 torangan Exp $
 //
 //  Pingus - A free Lemmings clone
 //  Copyright (C) 1999 Ingo Ruhnke <grumbel@gmx.de>
@@ -102,9 +102,6 @@ public:
 
   /// True if Pingu in specified position would bang its head if it were walking
   bool head_collision_on_walk (int x, int y);
-
-  void move_with_forces (float x_to_add, float y_to_add);
-
   
   /** Called if the action was successfully applied via request_set_action */
   virtual void on_successfull_apply () { }
