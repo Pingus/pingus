2ab943aa943e52c83c2c64e24dcc7164d4185bdd
Ingo Ruhnke <grumbel@gmail.com>
2003-02-16 18:32:44 +0000

    renamed configure.in to configure.ac, some cleanup

--- autogen.sh
+++ autogen.sh
@@ -1,9 +1,25 @@
 #!/bin/sh
 
-rm -f config.cache
-aclocal
+set -e
+
+AUTOMAKE=""
+ACLOCAL=""
+
+if `which automake-1.7`; then
+    AUTOMAKE="automake-1.7"
+    ACLOCAL="aclocal-1.7"
+elif `which automake-1.6`; then
+    AUTOMAKE="automake-1.6"
+    ACLOCAL="aclocal-1.6"
+else
+    AUTOMAKE="automake"
+    ACLOCAL="aclocal"
+fi
+
+echo "Generating ./configure, this may take a while..."
+$ACLOCAL
+$AUTOMAKE --force-missing --add-missing --copy
 autoheader
-automake --add-missing
 autoconf
 
 # EOF #
