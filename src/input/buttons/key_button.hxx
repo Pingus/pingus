fd509a717501ca4a801846315d8fd0c860be068b
David Philippi <david@torangan.de>
2003-06-16 20:59:17 +0000

    remove unrequired include

--- src/input/buttons/key_button.hxx
+++ src/input/buttons/key_button.hxx
@@ -1,4 +1,4 @@
-//  $Id: key_button.hxx,v 1.5 2003/06/16 20:58:11 torangan Exp $
+//  $Id: key_button.hxx,v 1.6 2003/06/16 20:59:17 torangan Exp $
 //
 //  Pingus - A free Lemmings clone
 //  Copyright (C) 2000 Ingo Ruhnke <grumbel@gmx.de>
@@ -20,7 +20,6 @@
 #ifndef HEADER_PINGUS_INPUT_KEY_BUTTON_HXX
 #define HEADER_PINGUS_INPUT_KEY_BUTTON_HXX
 
-#include <ClanLib/Display/Signals/slot.h>
 #include "../button.hxx"
 
 namespace Input {
