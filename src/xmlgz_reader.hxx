9e563248ed8f5a5a733af4676ae4144beb3ce889
David Philippi <david@torangan.de>
2004-04-13 16:47:39 +0000

    add support for gzip compressed levels

---
+++ src/xmlgz_reader.hxx
@@ -0,0 +1,35 @@
+/*  $Id: xml_plf.cxx,v 1.38 2003/04/19 10:23:17 torangan Exp $
+ *
+ *  Pingus - A free Lemmings clone
+ *  Copyright (C) 2000 Ingo Ruhnke <grumbel@gmx.de>
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+#ifndef HEADER_PINGUS_XMLGZ_READER_HXX
+#define HEADER_PINGUS_XMLGZ_READER_HXX
+
+#include "xml_helper.hxx"
+
+namespace Pingus {
+
+xmlDocPtr xmlgzParseFile(const char * filename);
+
+} // namespace Pingus
+
+#endif // Double-inclusion protection
+
+/* EOF */
+
