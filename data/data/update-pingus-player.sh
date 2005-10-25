a67610b46c3a5806b1fd8ff7ff45a9b59c0439dd
David Philippi <david@torangan.de>
2005-10-25 16:02:32 +0000

    applied multiplayer patch from Jason Green

---
+++ data/data/update-pingus-player.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+sed "s/player0/player1/" pingus-player0.xml > pingus-player1.xml
+sed "s/player0/player2/" pingus-player0.xml > pingus-player2.xml
+sed "s/player0/player3/" pingus-player0.xml > pingus-player3.xml
+#!/bin/sh
+
+sed "s/player0/player1/" pingus-player0.xml > pingus-player1.xml
+sed "s/player0/player2/" pingus-player0.xml > pingus-player2.xml
+sed "s/player0/player3/" pingus-player0.xml > pingus-player3.xml
