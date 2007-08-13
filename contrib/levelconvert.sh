a6dbf650641c6fa4427209b8c68c56e949b196be
Ingo Ruhnke <grumbel@gmail.com>
2007-08-13 21:29:44 +0000

    - level convert script, that does the xmlv1->xmlv2->scm in one go

---
+++ contrib/levelconvert.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+set -e
+trap errmsg ERR
+
+function errmsg() {
+    echo "ERROR: Critical failure :ERROR"
+}
+
+if [ ! \( -d "branches/pingus_sdl" \) ]; then
+    echo "You must call this script from the top level of the SVN repository"
+    exit 1
+fi
+
+for IN in "$@"; do
+    PREFIX=$(echo $IN | sed "s/\(.*\)data\/levels\/.*/\1/")
+    TMP=$(tempfile)
+    OUT="branches/pingus_sdl/${IN##${PREFIX}}"
+    OUT="${OUT%%.pingus}.scm"
+    # echo "Prefix: $PREFIX"
+    echo "IN:     $IN"
+    echo "OUT:    $OUT"
+    if true; then
+        xsltproc \
+            -o "$TMP" \
+            branches/pingus_sdl/contrib/pingusv1tov2.xsl \
+            "$IN"
+        branches/pingus_sdl/xml2sexpr.rb "$TMP" "$IN" > "${OUT}"        
+    else
+        xalan \
+            -indent 0 \
+            -xsl branches/pingus_sdl/contrib/pingusv1tov2.xsl \
+            -in "$IN" \
+            -out "$TMP"
+        branches/pingus_sdl/xml2sexpr.rb "$TMP" "$IN" > "${OUT}"
+    fi
+    echo "Conversion ok"
+    echo ""
+done
+
+# EOF #
