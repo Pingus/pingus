3301e39d9b2cdc0205d3a18cfafc67a27ef767e6
Ingo Ruhnke <grumbel@gmail.com>
2007-08-16 14:27:56 +0000

    - fixed a few bugs, added line numbers to .pot file

--- data/po/extract-po.sh
+++ data/po/extract-po.sh
@@ -3,24 +3,44 @@
 set -e
 
 TMP=$(tempfile)
-OUT="pingus.pot"
+OUT="data/po/pingus.pot"
 
-echo -n "Generating file list... "
-find ../../src/ -name "*.?pp" | sed "s/..\/..\///" > "$TMP"
+cd ../.. # go to toplevel dir
+echo -n "Generating C++ file list... "
+find src/ -name "*.?pp" > "$TMP"
 echo "done"
 
-echo -n "Extracting strings..."
+echo -n "Extracting strings from C++..."
 xgettext \
     --c++ \
     --boost \
-    --directory "../../" \
-    --output "$OUT" \
+    --directory "." \
+    --output "${OUT}.tmp" \
+    --from=code=ASCII \
     --keyword=_ \
     --keyword=N_ \
     --files-from "$TMP"
 echo "done"
 
-echo "Output written to $OUT"
+echo "" >> "${OUT}.tmp"
+
+echo -n "Extracting strings from levels... "
+data/po/extract-levels.guile data/levels/*/*.pingus >> "${OUT}.tmp"
+echo "done"
+
+echo -n "Extracting strings from worldmaps... "
+data/po/extract-levels.guile data/worldmaps/*.worldmap >> "${OUT}.tmp"
+echo "done"
+
+rpl "\"Content-Type: text/plain; charset=CHARSET\\n\"" \
+    "\"Content-Type: text/plain; charset=ASCII\\n\"" \
+    "${OUT}.tmp"
+
+echo -n "Removing duplicate entries... "
+msguniq -o "$OUT" "${OUT}.tmp"
+echo "done"
+
+echo "Output written to pingus.pot"
 
 rm $TMP
 
