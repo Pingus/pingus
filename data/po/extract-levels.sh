d6a9553601d599f81b8359fc05019ff6151706c6
Ingo Ruhnke <grumbel@gmail.com>
2007-08-16 04:39:58 +0000

    - extended script to handle worldmaps - renamed page[0-9][0-9] to just page in .worldmap

--- data/po/extract-levels.sh
+++ data/po/extract-levels.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
 set -e 
+
+echo -n "Extracting strings from levels... "
 ./extract-levels.guile ../levels/*/*.pingus > levels.pot
+echo "done"
+
+echo -n "Extracting strings from worldmaps... "
+./extract-levels.guile ../worldmaps/*.worldmap > worldmaps.pot
+echo "done"
 
 # EOF #
