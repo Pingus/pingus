c338d2f788ec0dafc5d1d44a8d4e92858837254f
Ingo Ruhnke <grumbel@gmail.com>
2007-08-16 14:35:23 +0000

    - updated pingus.pot

--- data/po/extract-po.sh
+++ data/po/extract-po.sh
@@ -32,8 +32,7 @@ echo -n "Extracting strings from worldmaps... "
 data/po/extract-levels.guile data/worldmaps/*.worldmap >> "${OUT}.tmp"
 echo "done"
 
-rpl "\"Content-Type: text/plain; charset=CHARSET\\n\"" \
-    "\"Content-Type: text/plain; charset=ASCII\\n\"" \
+sed -i "s/\"Content-Type: text\\/plain; charset=CHARSET\\n\\/\"Content-Type: text\\/plain; charset=ASCII\\n\"" \
     "${OUT}.tmp"
 
 echo -n "Removing duplicate entries... "
