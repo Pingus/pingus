6783835bff028bfdb3117992ce730754ad79f123
Ingo Ruhnke <grumbel@gmail.com>
2007-08-23 16:06:34 +0000

    - moved wiki_backup out of the source tree

--- doc/wiki_backup/download-wiki.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-wget --reject=.zip,.exe,.diff,.patch,.pingus,.png -domains=cmhousing.net \
-     --recursive --level=0 --convert-links -v http://www.cmhousing.net/pingus/wiki
-find -type f -name "*action=*" -exec rm -rf \{\} \;
-find -type f -name "Search@*" -exec rm -rf \{\} \;
-rm -f mains=*
-
