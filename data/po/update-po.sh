a8c31a057165811cd4bc63892f65c7ea7d5a87b9
Ingo Ruhnke <grumbel@gmail.com>
2007-10-09 03:56:11 +0000

    - added arguments

--- data/po/update-po.sh
+++ data/po/update-po.sh
@@ -1,8 +1,15 @@
-#!/bin/sh
+#!/bin/bash
 
-for i in *.po; do
-    echo -n "Updating $i:"
-    msgmerge --update "$i" pingus.pot
-done
+if [ $# -eq 0 ]; then 
+    for i in *.po; do
+        echo -n "Updating $i:"
+        msgmerge --update "$i" pingus.pot
+    done
+else
+    for i; do
+        echo -n "Updating $i:"
+        msgmerge --update "$i" pingus.pot
+    done    
+fi
 
 # EOF #
