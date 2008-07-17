bc97529354031b5a099106f6a8a0d8aa708d1d0f
Ingo Ruhnke <grumbel@gmail.com>
2008-07-17 09:24:51 +0000

    Added little debugging aid that allows to clear the screen to see what regions of the screen get updates (press Ctrl-m then 'k')

--- src/global_event.cpp
+++ src/global_event.cpp
@@ -77,6 +77,16 @@ GlobalEvent::on_button_press(const SDL_KeyboardEvent& event)
           draw_collision_map = !draw_collision_map;
         break;
 
+      case SDLK_k:
+        if (maintainer_mode)
+          {
+            std::cout << "Low level screen clear triggered" << std::endl;
+            SDL_Surface* screen = SDL_GetVideoSurface();
+            SDL_FillRect(screen, NULL, SDL_MapRGB(screen->format, 255, 255, 0));
+            SDL_Flip(screen);
+          }
+        break;
+
       case SDLK_m:
         if (keystate[SDLK_LCTRL] || keystate[SDLK_RCTRL])
           {
