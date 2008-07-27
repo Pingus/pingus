d34265c754ca570ddfbe977becd38b46bed76e05
Ingo Ruhnke <grumbel@gmail.com>
2008-07-27 01:42:43 +0000

    Added missing header

--- src/display/rect_merger.cpp
+++ src/display/rect_merger.cpp
@@ -15,6 +15,7 @@
 //  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 //#include <ext/algorithm>
+#include <algorithm>
 #include <assert.h>
 #include <iostream>
 #include "../math/vector2i.hpp"
