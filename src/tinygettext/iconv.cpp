1742492cbe9ce99fb2becf97112623c60ad5c1a2
Ingo Ruhnke <grumbel@gmail.com>
2011-05-12 15:05:09 +0000

    Some more portability stuff

--- src/tinygettext/iconv.cpp
+++ src/tinygettext/iconv.cpp
@@ -83,7 +83,7 @@ IConv::convert(std::string text)
   size_t in_size = text.size();
   size_t out_size = 4*in_size; // Worst case scenario: ASCII -> UTF-32?
   std::string result(out_size, ' ');
-  ICONV_CONST char* in_str  = &text[0];
+  const char* in_str  = &text[0];
   char* out_str = &result[0];
  
   // Try to convert the text.
