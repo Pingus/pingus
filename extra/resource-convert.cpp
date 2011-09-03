3ae75a3771ec5791c02409d757e4def81842ce8e
Ingo Ruhnke <grumbel@gmail.com>
2011-09-03 14:05:25 +0200

    Improved resource-convert script, cuts out unneeded directory specification and default fields

--- extra/resource-convert.cpp
+++ extra/resource-convert.cpp
@@ -38,22 +38,31 @@ int main(int argc, char** argv)
         throw std::runtime_error(resname + ": unexpected SpriteDescription");
       }
 
-      std::string base = System::basename(desc.filename.get_raw_path());
-      base = base.substr(0, base.size() - 4);
-      std::string dir = System::dirname(desc.filename.get_raw_path());
+      std::string image_file = System::normalize_path(desc.filename.get_raw_path());
+      std::string outfile = "data/images/" + resname + ".sprite";
 
-      // FIXME: must make sure that resname and dir+/+base are the
-      // same or otherwise adjust the image file path
-
-      std::string outfile = "data/" + resname + ".sprite";
+      // if directory is the same, cut it out
+      if (System::dirname(outfile) == ("data/" + System::dirname(image_file)))
+      {
+        image_file = System::basename(image_file);
+      }
+      else if (System::dirname(System::dirname(outfile)) == 
+               ("data/" + System::dirname(image_file)))
+      {
+        image_file = "../" + System::basename(image_file);
+      }
+      else
+      {
+        image_file = "/" + image_file;
+      }
 
       std::cout << resname << std::endl;
-      std::cout << "     default : " << (is_default_sprite(desc) ? "DEFAULT" : "regular") << std::endl;
       std::cout << "     outfile : " << outfile << std::endl;
-      //std::cout << "    basename : " << base << std::endl;
-      //std::cout << "    dirname  : " << dir << std::endl;
-      std::cout << "    filename : " << desc.filename << std::endl;
-      std::cout << "     offset  : " << desc.offset << std::endl;
+      std::cout << "     default : " << (is_default_sprite(desc) ? "DEFAULT" : "regular") << std::endl;
+      std::cout << "  image_file : " << image_file << std::endl;
+      std::cout << "     resname : " << resname << std::endl;
+      std::cout << "    filename : " << desc.filename.get_raw_path() << std::endl;
+      std::cout << "      offset : " << desc.offset << std::endl;
       std::cout << "      origin : " << desc.origin << std::endl;
       std::cout << "        loop : " << desc.loop << std::endl;
       std::cout << "       speed : " << desc.speed << std::endl;
@@ -62,9 +71,10 @@ int main(int argc, char** argv)
       std::cout << "  frame_size : " << desc.frame_size << std::endl;
       std::cout << std::endl;
       
-      if (false)
       if (!is_default_sprite(desc))
       {
+        System::create_dir(System::dirname(outfile));
+
         std::ofstream out(outfile);
         if (!out)
         {
@@ -74,14 +84,26 @@ int main(int argc, char** argv)
         {
           SExprFileWriter writer(out);
           writer.begin_section("pingus-sprite");
-          writer.write_int      ("speed",  desc.speed);
-          writer.write_bool     ("loop",   desc.loop);
-          writer.write_vector2i ("offset", desc.offset);
+          writer.write_string   ("image-file",  image_file);
+          if (desc.offset != Vector2i(0,0))
+            writer.write_vector2i ("offset", desc.offset);
+
+          if (desc.origin != origin_top_left)
           writer.write_enum     ("origin", desc.origin, origin2string);
-          writer.write_string   ("image-file",  System::basename(desc.filename.get_raw_path()));
-          writer.write_size     ("image-array", desc.array);
-          writer.write_vector2i ("image-pos",   desc.frame_pos);
-          writer.write_size     ("image-size",  desc.frame_size);
+
+          if (desc.frame_pos != Vector2i(0, 0))
+            writer.write_vector2i ("image-pos",   desc.frame_pos);
+
+          if (desc.array != Size(1, 1))
+          {
+            writer.write_int ("speed",  desc.speed);
+            writer.write_bool("loop",   desc.loop);
+            writer.write_size("image-array", desc.array);
+          }
+
+          if (desc.frame_size != Size(-1, -1)) 
+            writer.write_size     ("image-size",  desc.frame_size);
+
           writer.end_section();
           out << "\n\n;; EOF ;;" << std::endl;
         }
