6b6e1e9a0450867076a96883e2561386d1897566
Ingo Ruhnke <grumbel@gmail.com>
2011-09-25 01:11:03 +0200

    Added MessageBox

---
+++ src/editor/message_box.cpp
@@ -0,0 +1,107 @@
+//  Pingus - A free Lemmings clone
+//  Copyright (C) 1998-2011 Ingo Ruhnke <grumbel@gmx.de>
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#include "editor/message_box.hpp"
+
+#include "editor/gui_style.hpp"
+#include "pingus/gettext.h"
+#include "pingus/fonts.hpp"
+#include "util/log.hpp"
+
+namespace Editor {
+
+MessageBox::MessageBox(const Rect& rect_) :
+  GroupComponent(rect_),
+  m_ok_button(),
+  m_cancel_button(),
+  m_title(),
+  m_text(),
+  on_ok()
+{
+  m_cancel_button = new Button(Rect(Vector2i(rect.get_width() - 4 - 210, rect.get_height() - 4 - 30),
+                                    Size(100, 30)), _("Cancel"));
+  m_ok_button = new Button(Rect(Vector2i(rect.get_width() - 4 - 100, rect.get_height() - 4 - 30),
+                                Size(100, 30)), _("Replace"));
+
+  add(m_ok_button);
+  add(m_cancel_button);
+
+  m_ok_button->on_click.connect(std::bind(&MessageBox::on_ok_button, this));
+  m_cancel_button->on_click.connect(std::bind(&MessageBox::on_cancel_button, this));
+}
+
+void
+MessageBox::set_title(const std::string& text)
+{
+  m_title = text;
+}
+
+void
+MessageBox::set_text(const std::string& text)
+{
+  m_text = text;
+}
+
+void
+MessageBox::set_ok_text(const std::string& text)
+{
+  m_ok_button->set_text(text);
+}
+
+void
+MessageBox::draw_background(DrawingContext& gc)
+{
+  // Window border and title 
+  GUIStyle::draw_raised_box(gc, Rect(0,0,rect.get_width(), rect.get_height()));
+  gc.draw_fillrect(Rect(4,4,rect.get_width()-4, 30), Color(77,130,180));
+  gc.print_center(Fonts::pingus_small, Vector2i(rect.get_width()/2, 2), 
+                  m_title);
+
+  // main text
+  gc.print_center(Fonts::verdana11, Vector2i(rect.get_width()/2, 42), 
+                  m_text);
+}
+
+void
+MessageBox::update_layout()
+{
+  log_tmp("MessageBox::update_layout()");
+  GUI::GroupComponent::update_layout();
+
+  m_cancel_button->set_rect(Rect(Vector2i(rect.get_width() - 4 - 210, rect.get_height() - 4 - 30),
+                                 Size(100, 30)));
+  m_ok_button->set_rect(Rect(Vector2i(rect.get_width() - 4 - 100, rect.get_height() - 4 - 30),
+                             Size(100, 30)));
+}
+
+void
+MessageBox::on_ok_button()
+{
+  log_tmp("ok button");
+  hide();
+  on_ok();
+}
+
+void
+MessageBox::on_cancel_button()
+{
+  log_tmp("cancel button");
+  hide();
+}
+
+} // namespace Editor
+
+/* EOF */
---
+++ src/editor/message_box.hpp
@@ -0,0 +1,59 @@
+//  Pingus - A free Lemmings clone
+//  Copyright (C) 1998-2011 Ingo Ruhnke <grumbel@gmx.de>
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#ifndef HEADER_PINGUS_EDITOR_MESSAGE_BOX_HPP
+#define HEADER_PINGUS_EDITOR_MESSAGE_BOX_HPP
+
+#include "engine/gui/group_component.hpp"
+#include "editor/button.hpp"
+
+namespace Editor {
+
+class MessageBox : public GUI::GroupComponent
+{
+private:
+  Button* m_ok_button;
+  Button* m_cancel_button;
+
+  std::string m_title;
+  std::string m_text;
+
+public:
+  MessageBox(const Rect& rect);
+
+  void draw_background(DrawingContext& gc);
+  void update_layout();
+
+  void set_title(const std::string& text);
+  void set_text(const std::string& text);
+  void set_ok_text(const std::string& text);
+
+  void on_ok_button();
+  void on_cancel_button();
+
+public:
+  boost::signal<void()> on_ok;
+
+private:
+  MessageBox(const MessageBox&);
+  MessageBox& operator=(const MessageBox&);
+};
+
+} // namespace Editor
+
+#endif
+
+/* EOF */
