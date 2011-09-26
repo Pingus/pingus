5fa1928b4b27569f3043e85c3958c4ab6c12c27d
Ingo Ruhnke <grumbel@gmail.com>
2011-09-26 22:32:58 +0200

    Tests for Pathname::opendir() and Pathname::opendir_recursive()

---
+++ test/pathname_opendir_recursive_util.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+#include "pingus/path_manager.hpp"
+#include "util/pathname.hpp"
+
+int main(int argc, char** argv)
+{
+  g_path_manager.set_path("data");
+  for(int i = 1; i < argc; ++i)
+  {
+    g_path_manager.add_overlay_path(argv[i]);
+  }
+
+  Pathname path("images", Pathname::DATA_PATH);
+  auto lst = path.opendir_recursive();
+  for(auto it = lst.begin(); it != lst.end(); ++it)
+  {
+    std::cout << *it << std::endl;
+  }
+  return 0;
+}
+
+/* EOF */
