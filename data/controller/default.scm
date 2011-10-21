dab3f156a326756205043b3bfe06bcc310d226c9
Ingo Ruhnke <grumbel@gmail.com>
2011-10-21 19:14:48 +0200

    Changed default keymap to be centered around WASD

--- data/controller/default.scm
+++ data/controller/default.scm
@@ -10,7 +10,13 @@
     (up    (sdl:keyboard-button (key "up")))
     (down  (sdl:keyboard-button (key "down")))
     (left  (sdl:keyboard-button (key "left")))
-    (right (sdl:keyboard-button (key "right")))))
+    (right (sdl:keyboard-button (key "right"))))
+
+   (core:button-scroller
+    (up    (sdl:keyboard-button (key "w")))
+    (down  (sdl:keyboard-button (key "s")))
+    (left  (sdl:keyboard-button (key "a")))
+    (right (sdl:keyboard-button (key "d")))))
 
   (primary-button
     (sdl:mouse-button (button 1)))
@@ -20,28 +26,31 @@
     (sdl:mouse-button (button 3)))
 
   (pause-button
+   (sdl:keyboard-button (key "r"))
    (sdl:keyboard-button (key "p")))
 
   (fast-forward-button
-   (sdl:keyboard-button (key "f"))
    (sdl:keyboard-button (key "space")))
 
   (single-step-button
-   (sdl:keyboard-button (key "s")))
+   (sdl:keyboard-button (key "s"))
+   (sdl:keyboard-button (key "f")))
 
   (armageddon-button
-   (sdl:keyboard-button (key "a")))
+   (sdl:keyboard-button (key "backspace")))
 
   (escape-button
    (sdl:keyboard-button (key "escape")))
 
   (action-up-button
    (sdl:keyboard-button (key "q"))
+   (sdl:keyboard-button (key "page up"))
    (sdl:mouse-button (button 4)))
   
   (action-down-button
    (sdl:keyboard-button (key "e"))
    (sdl:keyboard-button (key "tab"))
+   (sdl:keyboard-button (key "page down"))
    (sdl:mouse-button (button 5)))
 
   (action-1-button  (sdl:keyboard-button (key "1")))
