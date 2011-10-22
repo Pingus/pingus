965caca6ee35d333197553eda16652e49fbef4d2
Ingo Ruhnke <grumbel@gmail.com>
2011-10-23 01:50:29 +0200

    Moved back to old control scheme, WASD is better, but causes conflicts with the editor

--- data/controller/default.scm
+++ data/controller/default.scm
@@ -10,13 +10,7 @@
     (up    (sdl:keyboard-button (key "up")))
     (down  (sdl:keyboard-button (key "down")))
     (left  (sdl:keyboard-button (key "left")))
-    (right (sdl:keyboard-button (key "right"))))
-
-   (core:button-scroller
-    (up    (sdl:keyboard-button (key "w")))
-    (down  (sdl:keyboard-button (key "s")))
-    (left  (sdl:keyboard-button (key "a")))
-    (right (sdl:keyboard-button (key "d")))))
+    (right (sdl:keyboard-button (key "right")))))
 
   (primary-button
     (sdl:mouse-button (button 1)))
@@ -26,30 +20,26 @@
     (sdl:mouse-button (button 3)))
 
   (pause-button
-   (sdl:keyboard-button (key "r"))
    (sdl:keyboard-button (key "p")))
 
   (fast-forward-button
+   (sdl:keyboard-button (key "f"))
    (sdl:keyboard-button (key "space")))
 
   (single-step-button
-   (sdl:keyboard-button (key "f")))
+   (sdl:keyboard-button (key "s")))
 
   (armageddon-button
-   (sdl:keyboard-button (key "backspace")))
+   (sdl:keyboard-button (key "a")))
 
   (escape-button
    (sdl:keyboard-button (key "escape")))
 
   (action-up-button
-   (sdl:keyboard-button (key "q"))
-   (sdl:keyboard-button (key "page up"))
    (sdl:mouse-button (button 4)))
   
   (action-down-button
-   (sdl:keyboard-button (key "e"))
    (sdl:keyboard-button (key "tab"))
-   (sdl:keyboard-button (key "page down"))
    (sdl:mouse-button (button 5)))
 
   (action-1-button  (sdl:keyboard-button (key "1")))
