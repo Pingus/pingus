7fea522b4749ed96393e3686bd5f18263f47548c
Josh Dye <josh.jpenguin@gmail.com>
2011-12-27 18:41:51 -0800

    More files to allow for MacOS X bundling

---
+++ bundle_mac.sh
@@ -0,0 +1,17 @@
+echo "* Removing any existing installation"
+rm -rf ./Pingus.app
+
+echo "* Creating skeleton"
+mkdir -p ./Pingus.app/Contents/Resources
+mkdir ./Pingus.app/Contents/MacOS
+cp info.plist ./Pingus.app/Contents/info.plist
+cp ./data/images/icons/pingus-icon.icns ./Pingus.app/Contents/Resources/pingus.icns
+
+echo "* Copying executable"
+cp ./build/pingus ./Pingus.app/Contents/MacOS/pingus
+
+echo "* Copying data files"
+cp -r ./data/* ./Pingus.app/Contents/Resources/
+
+echo "* Bunling dependencies"
+dylibbundler -od -b -x ./Pingus.app/Contents/MacOS/lincity-ng -d ./Pingus.app/Contents/libs/
