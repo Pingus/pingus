f71d302a97c0cfa0d4b3b515307a6f30bf779788
Ingo Ruhnke <grumbel@gmail.com>
2015-05-31 10:50:27 +0200

    Adjusted space in copyright header

--- src/editor/viewport.hpp
+++ src/editor/viewport.hpp
@@ -1,19 +1,19 @@
-//  Pingus - A free Lemmings clone
-//  Copyright (C) 2007 Jason Green <jave27@gmail.com>,
-//                     Ingo Ruhnke <grumbel@gmail.com>
+// Pingus - A free Lemmings clone
+// Copyright (C) 2007 Jason Green <jave27@gmail.com>,
+//                    Ingo Ruhnke <grumbel@gmail.com>
 //
-//  This program is free software: you can redistribute it and/or modify
-//  it under the terms of the GNU General Public License as published by
-//  the Free Software Foundation, either version 3 of the License, or
-//  (at your option) any later version.
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
 //
-//  This program is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//  GNU General Public License for more details.
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
 //
-//  You should have received a copy of the GNU General Public License
-//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #ifndef HEADER_PINGUS_EDITOR_VIEWPORT_HPP
 #define HEADER_PINGUS_EDITOR_VIEWPORT_HPP
