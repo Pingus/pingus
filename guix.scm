5b9ffa9fb2be7ec3dbe8c98ea1a680c24700f3f1
Ingo Ruhnke <grumbel@gmail.com>
2019-11-21 14:10:30 +0100

    Unclutter guix.scm, add IPFS support

--- guix.scm
+++ guix.scm
@@ -14,83 +14,53 @@
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+(set! %load-path
+  (cons* "/ipfs/QmaCGPPoTngVcz6kTCVgnkPnVDxUE89A17kiiuY7gcWVi6/guix-cocfree_v0.0.0-44-g80fb507"
+         %load-path))
+
 (use-modules (guix build-system cmake)
-             (guix gexp)
-             (guix git-download)
-             (guix licenses)
+             ((guix licenses) #:prefix license:)
              (guix packages)
              (gnu packages audio)
              (gnu packages boost)
              (gnu packages freedesktop)
-             (gnu packages game-development)
              (gnu packages gcc)
              (gnu packages gl)
              (gnu packages glib)
              (gnu packages image)
              (gnu packages pkg-config)
-             (gnu packages python-xyz)
              (gnu packages sdl)
-             (gnu packages serialization))
+             (gnu packages serialization)
+             (guix-cocfree utils))
 
 (define %source-dir (dirname (current-filename)))
-
-(define (source-predicate . dirs)
-  (let ((preds (map (lambda (p)
-                      (git-predicate (string-append %source-dir p)))
-                    dirs)))
-    (lambda (file stat)
-      (let loop ((f (car preds))
-                 (rest (cdr preds)))
-        (if (f file stat)
-            #t
-            (if (not (nil? rest))
-                (loop (car rest) (cdr rest))
-                #f))))))
 
 (define-public pingus
   (package
    (name "pingus")
-   (version "0.8.0")
-   (source (local-file %source-dir
-                       #:recursive? #t
-                       #:select? (source-predicate ""
-                                                   "/external/googletest"
-                                                   "/external/logmich"
-                                                   "/external/sexp-cpp"
-                                                   "/external/tinygettext"
-                                                   "/external/uitest"
-                                                   "/external/xdg"
-                                                   )))
+   (version (version-from-source %source-dir))
+   (source (source-from-source %source-dir))
    (build-system cmake-build-system)
    (arguments
-    `(#:tests? #f ; network access needed for URL checks
-      #:configure-flags (list "-DBUILD_TESTS=ON"
-                              "-DBUILD_EXTRA=ON")))
-   (native-inputs `(("pkg-config" ,pkg-config)
-                    ("desktop-file-utils" ,desktop-file-utils)
-                    ("appstream-glib" ,appstream-glib)
-                    ("gcc-9" ,gcc-9))) ; needed for <filesystem>
-   (propagated-inputs
-    `(("libmodplug" ,libmodplug))) ; FIXME: needed for sound, but not found by SDL_Mixer
-   (inputs `(("sdl2" ,sdl)
-             ("sdl2-image" ,sdl2-image)
-             ("sdl2-mixer" ,sdl2-mixer)
-             ("mesa" ,mesa)
-             ("glu" ,glu)
-             ("libpng" ,libpng)
-             ("jsoncpp" ,jsoncpp)
-             ("boost" ,boost)))
-   (synopsis "Guide groups of penguins through various obstacles")
-   (description "A 2d side scrolling puzzle game where you guide large
-groups of little penguins through various obstacles and dangers using
-their numerous talents.
-
-A level editor is included to allow building your own levels and
-numerous user contributed levels are included as well.
-
-The game is heavily inspired by the classic Lemmings.")
-   (home-page "https://pingus.seul.org")
-   (license gpl3+)))
+    `(#:tests? #f)) ; network access needed for URL checks
+   (native-inputs
+    `(("pkg-config" ,pkg-config)
+      ("desktop-file-utils" ,desktop-file-utils)
+      ("appstream-glib" ,appstream-glib)
+      ("gcc-9" ,gcc-9))) ; needed for <filesystem>
+   (inputs
+    `(("sdl2" ,sdl)
+      ("sdl2-image" ,sdl2-image)
+      ("sdl2-mixer" ,sdl2-mixer)
+      ("mesa" ,mesa)
+      ("glu" ,glu)
+      ("libpng" ,libpng)
+      ("jsoncpp" ,jsoncpp)
+      ("boost" ,boost)))
+   (synopsis (synopsis-from-source %source-dir))
+   (description (description-from-source %source-dir))
+   (home-page (homepage-from-source %source-dir))
+   (license license:gpl3+)))
 
 pingus
 
