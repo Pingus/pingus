d43c5e9fd50285b37d23dc5fa661d39e309d9450
Ingo Ruhnke <grumbel@gmail.com>
2020-08-31 00:29:45 +0200

    Update guix.scm

--- guix.scm
+++ guix.scm
@@ -15,20 +15,20 @@
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 (set! %load-path
-  (cons* "/ipfs/QmeUZkZxWMHKNR4HAGU9G2VRFcyiSFt3JNpuF5RBC9wKZa/guix-cocfree_0.0.0-51-gc2bce55"
+  (cons* "/ipfs/QmdHSWX34MXZeAgMuDuFwrYzSPf4fknX7E7cYJ7HDxaLrZ/guix-cocfree_0.0.0-62-g3b27118"
          %load-path))
 
 (use-modules (guix build-system cmake)
              ((guix licenses) #:prefix license:)
              (guix packages)
              (gnu packages audio)
-             (gnu packages boost)
              (gnu packages freedesktop)
              (gnu packages gcc)
              (gnu packages gl)
              (gnu packages glib)
              (gnu packages image)
              (gnu packages pkg-config)
+             (gnu packages pretty-print)
              (gnu packages sdl)
              (gnu packages serialization)
              (guix-cocfree utils))
@@ -47,16 +47,17 @@
     `(("pkg-config" ,pkg-config)
       ("desktop-file-utils" ,desktop-file-utils)
       ("appstream-glib" ,appstream-glib)
-      ("gcc-9" ,gcc-9))) ; needed for <filesystem>
+      ("gcc-10" ,gcc-10))) ; needed for <filesystem>
    (inputs
-    `(("sdl2" ,sdl)
+    `(("sdl2" ,sdl2)
       ("sdl2-image" ,sdl2-image)
       ("sdl2-mixer" ,sdl2-mixer)
       ("mesa" ,mesa)
       ("glu" ,glu)
       ("libpng" ,libpng)
-      ("jsoncpp" ,jsoncpp)
-      ("boost" ,boost)))
+      ("libsigc++" ,libsigc++)
+      ("fmt" ,fmt)
+      ("jsoncpp" ,jsoncpp)))
    (synopsis (synopsis-from-source %source-dir))
    (description (description-from-source %source-dir))
    (home-page (homepage-from-source %source-dir))
