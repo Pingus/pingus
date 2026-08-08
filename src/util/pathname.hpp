c9074fb1029a7a67c2711ee302acaf59b7435c47
Ingo Ruhnke <grumbel@gmail.com>
2026-08-08 21:38:48 +0000

    fix: keep Pathname std::formatter inside include guard
    Co-authored-by: Grok <grok@x.ai>

--- src/util/pathname.hpp
+++ src/util/pathname.hpp
@@ -1,26 +1,26 @@
-// Pingus - A free Lemmings clone
-// Copyright (C) 2007 Ingo Ruhnke <grumbel@gmail.com>
+//  Pingus - A free Lemmings clone
+//  Copyright (C) 2007 Ingo Ruhnke <grumbel@gmail.com>
 //
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
 //
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
 //
-// You should have received a copy of the GNU General Public License
-// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #ifndef HEADER_PINGUS_UTIL_PATHNAME_HPP
 #define HEADER_PINGUS_UTIL_PATHNAME_HPP
 
 #include <cstdint>
+#include <format>
 #include <string>
 #include <vector>
-#include <format>
 #include <sstream>
 
 namespace pingus {
@@ -91,10 +91,6 @@ std::ostream& operator<< (std::ostream& os, Pathname const& p);
 
 } // namespace pingus
 
-#endif
-
-/* EOF */
-
 template<>
 struct std::formatter<pingus::Pathname>
 {
@@ -106,3 +102,6 @@ struct std::formatter<pingus::Pathname>
   }
 };
 
+#endif
+
+/* EOF */
