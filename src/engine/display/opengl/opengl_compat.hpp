9deb1b2f7a74b9bacbbb9e8a4e3ff67584ebbf17
Ingo Ruhnke <grumbel@gmail.com>
2026-08-09 18:13:44 +0000

    win32: avoid clashing with SDL_opengl gl* declarations
    MinGW SDL_opengl.h already declares some modern GL entry points as
    functions. Keep runtime-loaded pointers under pingus_gl* names and
    macro-redirect gl* call sites so we do not redeclare the symbols.
    
    Co-authored-by: Grok <grok@x.ai>

--- src/engine/display/opengl/opengl_compat.hpp
+++ src/engine/display/opengl/opengl_compat.hpp
@@ -23,39 +23,70 @@
 #  include <SDL_opengl.h>
 
 #  if defined(_WIN32)
-// opengl32.dll only has the 1.1 ABI. Declare modern entry points as function
-// pointers loaded via SDL_GL_GetProcAddress (see opengl_wgl_load.cpp).
+// SDL_opengl.h (MinGW) already declares some 1.2+ entry points as functions,
+// but opengl32.dll does not provide them. Use uniquely named function pointers
+// loaded via SDL_GL_GetProcAddress, then macro-redirect the usual gl* names.
 #    include <SDL_opengl_glext.h>
 
-extern PFNGLACTIVETEXTUREPROC              glActiveTexture;
-extern PFNGLATTACHSHADERPROC               glAttachShader;
-extern PFNGLBINDBUFFERPROC                 glBindBuffer;
-extern PFNGLBINDVERTEXARRAYPROC            glBindVertexArray;
-extern PFNGLBUFFERDATAPROC                 glBufferData;
-extern PFNGLCOMPILESHADERPROC              glCompileShader;
-extern PFNGLCREATEPROGRAMPROC              glCreateProgram;
-extern PFNGLCREATESHADERPROC               glCreateShader;
-extern PFNGLDELETEBUFFERSPROC              glDeleteBuffers;
-extern PFNGLDELETEPROGRAMPROC              glDeleteProgram;
-extern PFNGLDELETESHADERPROC               glDeleteShader;
-extern PFNGLDELETEVERTEXARRAYSPROC         glDeleteVertexArrays;
-extern PFNGLDISABLEVERTEXATTRIBARRAYPROC   glDisableVertexAttribArray;
-extern PFNGLENABLEVERTEXATTRIBARRAYPROC    glEnableVertexAttribArray;
-extern PFNGLGENBUFFERSPROC                 glGenBuffers;
-extern PFNGLGENVERTEXARRAYSPROC            glGenVertexArrays;
-extern PFNGLGETATTRIBLOCATIONPROC          glGetAttribLocation;
-extern PFNGLGETPROGRAMINFOLOGPROC          glGetProgramInfoLog;
-extern PFNGLGETPROGRAMIVPROC               glGetProgramiv;
-extern PFNGLGETSHADERINFOLOGPROC           glGetShaderInfoLog;
-extern PFNGLGETSHADERIVPROC                glGetShaderiv;
-extern PFNGLGETUNIFORMLOCATIONPROC         glGetUniformLocation;
-extern PFNGLLINKPROGRAMPROC                glLinkProgram;
-extern PFNGLSHADERSOURCEPROC               glShaderSource;
-extern PFNGLUNIFORM1IPROC                  glUniform1i;
-extern PFNGLUNIFORM4FVPROC                 glUniform4fv;
-extern PFNGLUNIFORMMATRIX4FVPROC           glUniformMatrix4fv;
-extern PFNGLUSEPROGRAMPROC                 glUseProgram;
-extern PFNGLVERTEXATTRIBPOINTERPROC        glVertexAttribPointer;
+extern PFNGLACTIVETEXTUREPROC              pingus_glActiveTexture;
+extern PFNGLATTACHSHADERPROC               pingus_glAttachShader;
+extern PFNGLBINDBUFFERPROC                 pingus_glBindBuffer;
+extern PFNGLBINDVERTEXARRAYPROC            pingus_glBindVertexArray;
+extern PFNGLBUFFERDATAPROC                 pingus_glBufferData;
+extern PFNGLCOMPILESHADERPROC              pingus_glCompileShader;
+extern PFNGLCREATEPROGRAMPROC              pingus_glCreateProgram;
+extern PFNGLCREATESHADERPROC               pingus_glCreateShader;
+extern PFNGLDELETEBUFFERSPROC              pingus_glDeleteBuffers;
+extern PFNGLDELETEPROGRAMPROC              pingus_glDeleteProgram;
+extern PFNGLDELETESHADERPROC               pingus_glDeleteShader;
+extern PFNGLDELETEVERTEXARRAYSPROC         pingus_glDeleteVertexArrays;
+extern PFNGLDISABLEVERTEXATTRIBARRAYPROC   pingus_glDisableVertexAttribArray;
+extern PFNGLENABLEVERTEXATTRIBARRAYPROC    pingus_glEnableVertexAttribArray;
+extern PFNGLGENBUFFERSPROC                 pingus_glGenBuffers;
+extern PFNGLGENVERTEXARRAYSPROC            pingus_glGenVertexArrays;
+extern PFNGLGETATTRIBLOCATIONPROC          pingus_glGetAttribLocation;
+extern PFNGLGETPROGRAMINFOLOGPROC          pingus_glGetProgramInfoLog;
+extern PFNGLGETPROGRAMIVPROC               pingus_glGetProgramiv;
+extern PFNGLGETSHADERINFOLOGPROC           pingus_glGetShaderInfoLog;
+extern PFNGLGETSHADERIVPROC                pingus_glGetShaderiv;
+extern PFNGLGETUNIFORMLOCATIONPROC         pingus_glGetUniformLocation;
+extern PFNGLLINKPROGRAMPROC                pingus_glLinkProgram;
+extern PFNGLSHADERSOURCEPROC               pingus_glShaderSource;
+extern PFNGLUNIFORM1IPROC                  pingus_glUniform1i;
+extern PFNGLUNIFORM4FVPROC                 pingus_glUniform4fv;
+extern PFNGLUNIFORMMATRIX4FVPROC           pingus_glUniformMatrix4fv;
+extern PFNGLUSEPROGRAMPROC                 pingus_glUseProgram;
+extern PFNGLVERTEXATTRIBPOINTERPROC        pingus_glVertexAttribPointer;
+
+#define glActiveTexture            pingus_glActiveTexture
+#define glAttachShader             pingus_glAttachShader
+#define glBindBuffer               pingus_glBindBuffer
+#define glBindVertexArray          pingus_glBindVertexArray
+#define glBufferData               pingus_glBufferData
+#define glCompileShader            pingus_glCompileShader
+#define glCreateProgram            pingus_glCreateProgram
+#define glCreateShader             pingus_glCreateShader
+#define glDeleteBuffers            pingus_glDeleteBuffers
+#define glDeleteProgram            pingus_glDeleteProgram
+#define glDeleteShader             pingus_glDeleteShader
+#define glDeleteVertexArrays       pingus_glDeleteVertexArrays
+#define glDisableVertexAttribArray pingus_glDisableVertexAttribArray
+#define glEnableVertexAttribArray  pingus_glEnableVertexAttribArray
+#define glGenBuffers               pingus_glGenBuffers
+#define glGenVertexArrays          pingus_glGenVertexArrays
+#define glGetAttribLocation        pingus_glGetAttribLocation
+#define glGetProgramInfoLog        pingus_glGetProgramInfoLog
+#define glGetProgramiv             pingus_glGetProgramiv
+#define glGetShaderInfoLog         pingus_glGetShaderInfoLog
+#define glGetShaderiv              pingus_glGetShaderiv
+#define glGetUniformLocation       pingus_glGetUniformLocation
+#define glLinkProgram              pingus_glLinkProgram
+#define glShaderSource             pingus_glShaderSource
+#define glUniform1i                pingus_glUniform1i
+#define glUniform4fv               pingus_glUniform4fv
+#define glUniformMatrix4fv         pingus_glUniformMatrix4fv
+#define glUseProgram               pingus_glUseProgram
+#define glVertexAttribPointer      pingus_glVertexAttribPointer
 
 namespace pingus {
 void opengl_load_procs();
--- src/engine/display/opengl/opengl_wgl_load.cpp
+++ src/engine/display/opengl/opengl_wgl_load.cpp
@@ -11,35 +11,66 @@
 
 #include "util/raise_exception.hpp"
 
-PFNGLACTIVETEXTUREPROC              glActiveTexture = nullptr;
-PFNGLATTACHSHADERPROC               glAttachShader = nullptr;
-PFNGLBINDBUFFERPROC                 glBindBuffer = nullptr;
-PFNGLBINDVERTEXARRAYPROC            glBindVertexArray = nullptr;
-PFNGLBUFFERDATAPROC                 glBufferData = nullptr;
-PFNGLCOMPILESHADERPROC              glCompileShader = nullptr;
-PFNGLCREATEPROGRAMPROC              glCreateProgram = nullptr;
-PFNGLCREATESHADERPROC               glCreateShader = nullptr;
-PFNGLDELETEBUFFERSPROC              glDeleteBuffers = nullptr;
-PFNGLDELETEPROGRAMPROC              glDeleteProgram = nullptr;
-PFNGLDELETESHADERPROC               glDeleteShader = nullptr;
-PFNGLDELETEVERTEXARRAYSPROC         glDeleteVertexArrays = nullptr;
-PFNGLDISABLEVERTEXATTRIBARRAYPROC   glDisableVertexAttribArray = nullptr;
-PFNGLENABLEVERTEXATTRIBARRAYPROC    glEnableVertexAttribArray = nullptr;
-PFNGLGENBUFFERSPROC                 glGenBuffers = nullptr;
-PFNGLGENVERTEXARRAYSPROC            glGenVertexArrays = nullptr;
-PFNGLGETATTRIBLOCATIONPROC          glGetAttribLocation = nullptr;
-PFNGLGETPROGRAMINFOLOGPROC          glGetProgramInfoLog = nullptr;
-PFNGLGETPROGRAMIVPROC               glGetProgramiv = nullptr;
-PFNGLGETSHADERINFOLOGPROC           glGetShaderInfoLog = nullptr;
-PFNGLGETSHADERIVPROC                glGetShaderiv = nullptr;
-PFNGLGETUNIFORMLOCATIONPROC         glGetUniformLocation = nullptr;
-PFNGLLINKPROGRAMPROC                glLinkProgram = nullptr;
-PFNGLSHADERSOURCEPROC               glShaderSource = nullptr;
-PFNGLUNIFORM1IPROC                  glUniform1i = nullptr;
-PFNGLUNIFORM4FVPROC                 glUniform4fv = nullptr;
-PFNGLUNIFORMMATRIX4FVPROC           glUniformMatrix4fv = nullptr;
-PFNGLUSEPROGRAMPROC                 glUseProgram = nullptr;
-PFNGLVERTEXATTRIBPOINTERPROC        glVertexAttribPointer = nullptr;
+// Header macros would rename these identifiers; strip them for definitions.
+#undef glActiveTexture
+#undef glAttachShader
+#undef glBindBuffer
+#undef glBindVertexArray
+#undef glBufferData
+#undef glCompileShader
+#undef glCreateProgram
+#undef glCreateShader
+#undef glDeleteBuffers
+#undef glDeleteProgram
+#undef glDeleteShader
+#undef glDeleteVertexArrays
+#undef glDisableVertexAttribArray
+#undef glEnableVertexAttribArray
+#undef glGenBuffers
+#undef glGenVertexArrays
+#undef glGetAttribLocation
+#undef glGetProgramInfoLog
+#undef glGetProgramiv
+#undef glGetShaderInfoLog
+#undef glGetShaderiv
+#undef glGetUniformLocation
+#undef glLinkProgram
+#undef glShaderSource
+#undef glUniform1i
+#undef glUniform4fv
+#undef glUniformMatrix4fv
+#undef glUseProgram
+#undef glVertexAttribPointer
+
+PFNGLACTIVETEXTUREPROC              pingus_glActiveTexture = nullptr;
+PFNGLATTACHSHADERPROC               pingus_glAttachShader = nullptr;
+PFNGLBINDBUFFERPROC                 pingus_glBindBuffer = nullptr;
+PFNGLBINDVERTEXARRAYPROC            pingus_glBindVertexArray = nullptr;
+PFNGLBUFFERDATAPROC                 pingus_glBufferData = nullptr;
+PFNGLCOMPILESHADERPROC              pingus_glCompileShader = nullptr;
+PFNGLCREATEPROGRAMPROC              pingus_glCreateProgram = nullptr;
+PFNGLCREATESHADERPROC               pingus_glCreateShader = nullptr;
+PFNGLDELETEBUFFERSPROC              pingus_glDeleteBuffers = nullptr;
+PFNGLDELETEPROGRAMPROC              pingus_glDeleteProgram = nullptr;
+PFNGLDELETESHADERPROC               pingus_glDeleteShader = nullptr;
+PFNGLDELETEVERTEXARRAYSPROC         pingus_glDeleteVertexArrays = nullptr;
+PFNGLDISABLEVERTEXATTRIBARRAYPROC   pingus_glDisableVertexAttribArray = nullptr;
+PFNGLENABLEVERTEXATTRIBARRAYPROC    pingus_glEnableVertexAttribArray = nullptr;
+PFNGLGENBUFFERSPROC                 pingus_glGenBuffers = nullptr;
+PFNGLGENVERTEXARRAYSPROC            pingus_glGenVertexArrays = nullptr;
+PFNGLGETATTRIBLOCATIONPROC          pingus_glGetAttribLocation = nullptr;
+PFNGLGETPROGRAMINFOLOGPROC          pingus_glGetProgramInfoLog = nullptr;
+PFNGLGETPROGRAMIVPROC               pingus_glGetProgramiv = nullptr;
+PFNGLGETSHADERINFOLOGPROC           pingus_glGetShaderInfoLog = nullptr;
+PFNGLGETSHADERIVPROC                pingus_glGetShaderiv = nullptr;
+PFNGLGETUNIFORMLOCATIONPROC         pingus_glGetUniformLocation = nullptr;
+PFNGLLINKPROGRAMPROC                pingus_glLinkProgram = nullptr;
+PFNGLSHADERSOURCEPROC               pingus_glShaderSource = nullptr;
+PFNGLUNIFORM1IPROC                  pingus_glUniform1i = nullptr;
+PFNGLUNIFORM4FVPROC                 pingus_glUniform4fv = nullptr;
+PFNGLUNIFORMMATRIX4FVPROC           pingus_glUniformMatrix4fv = nullptr;
+PFNGLUSEPROGRAMPROC                 pingus_glUseProgram = nullptr;
+PFNGLVERTEXATTRIBPOINTERPROC        pingus_glVertexAttribPointer = nullptr;
 
 namespace pingus {
 
@@ -59,35 +90,35 @@ T load_proc(char const* name)
 
 void opengl_load_procs()
 {
-  glActiveTexture = load_proc<PFNGLACTIVETEXTUREPROC>("glActiveTexture");
-  glAttachShader = load_proc<PFNGLATTACHSHADERPROC>("glAttachShader");
-  glBindBuffer = load_proc<PFNGLBINDBUFFERPROC>("glBindBuffer");
-  glBindVertexArray = load_proc<PFNGLBINDVERTEXARRAYPROC>("glBindVertexArray");
-  glBufferData = load_proc<PFNGLBUFFERDATAPROC>("glBufferData");
-  glCompileShader = load_proc<PFNGLCOMPILESHADERPROC>("glCompileShader");
-  glCreateProgram = load_proc<PFNGLCREATEPROGRAMPROC>("glCreateProgram");
-  glCreateShader = load_proc<PFNGLCREATESHADERPROC>("glCreateShader");
-  glDeleteBuffers = load_proc<PFNGLDELETEBUFFERSPROC>("glDeleteBuffers");
-  glDeleteProgram = load_proc<PFNGLDELETEPROGRAMPROC>("glDeleteProgram");
-  glDeleteShader = load_proc<PFNGLDELETESHADERPROC>("glDeleteShader");
-  glDeleteVertexArrays = load_proc<PFNGLDELETEVERTEXARRAYSPROC>("glDeleteVertexArrays");
-  glDisableVertexAttribArray = load_proc<PFNGLDISABLEVERTEXATTRIBARRAYPROC>("glDisableVertexAttribArray");
-  glEnableVertexAttribArray = load_proc<PFNGLENABLEVERTEXATTRIBARRAYPROC>("glEnableVertexAttribArray");
-  glGenBuffers = load_proc<PFNGLGENBUFFERSPROC>("glGenBuffers");
-  glGenVertexArrays = load_proc<PFNGLGENVERTEXARRAYSPROC>("glGenVertexArrays");
-  glGetAttribLocation = load_proc<PFNGLGETATTRIBLOCATIONPROC>("glGetAttribLocation");
-  glGetProgramInfoLog = load_proc<PFNGLGETPROGRAMINFOLOGPROC>("glGetProgramInfoLog");
-  glGetProgramiv = load_proc<PFNGLGETPROGRAMIVPROC>("glGetProgramiv");
-  glGetShaderInfoLog = load_proc<PFNGLGETSHADERINFOLOGPROC>("glGetShaderInfoLog");
-  glGetShaderiv = load_proc<PFNGLGETSHADERIVPROC>("glGetShaderiv");
-  glGetUniformLocation = load_proc<PFNGLGETUNIFORMLOCATIONPROC>("glGetUniformLocation");
-  glLinkProgram = load_proc<PFNGLLINKPROGRAMPROC>("glLinkProgram");
-  glShaderSource = load_proc<PFNGLSHADERSOURCEPROC>("glShaderSource");
-  glUniform1i = load_proc<PFNGLUNIFORM1IPROC>("glUniform1i");
-  glUniform4fv = load_proc<PFNGLUNIFORM4FVPROC>("glUniform4fv");
-  glUniformMatrix4fv = load_proc<PFNGLUNIFORMMATRIX4FVPROC>("glUniformMatrix4fv");
-  glUseProgram = load_proc<PFNGLUSEPROGRAMPROC>("glUseProgram");
-  glVertexAttribPointer = load_proc<PFNGLVERTEXATTRIBPOINTERPROC>("glVertexAttribPointer");
+  pingus_glActiveTexture = load_proc<PFNGLACTIVETEXTUREPROC>("glActiveTexture");
+  pingus_glAttachShader = load_proc<PFNGLATTACHSHADERPROC>("glAttachShader");
+  pingus_glBindBuffer = load_proc<PFNGLBINDBUFFERPROC>("glBindBuffer");
+  pingus_glBindVertexArray = load_proc<PFNGLBINDVERTEXARRAYPROC>("glBindVertexArray");
+  pingus_glBufferData = load_proc<PFNGLBUFFERDATAPROC>("glBufferData");
+  pingus_glCompileShader = load_proc<PFNGLCOMPILESHADERPROC>("glCompileShader");
+  pingus_glCreateProgram = load_proc<PFNGLCREATEPROGRAMPROC>("glCreateProgram");
+  pingus_glCreateShader = load_proc<PFNGLCREATESHADERPROC>("glCreateShader");
+  pingus_glDeleteBuffers = load_proc<PFNGLDELETEBUFFERSPROC>("glDeleteBuffers");
+  pingus_glDeleteProgram = load_proc<PFNGLDELETEPROGRAMPROC>("glDeleteProgram");
+  pingus_glDeleteShader = load_proc<PFNGLDELETESHADERPROC>("glDeleteShader");
+  pingus_glDeleteVertexArrays = load_proc<PFNGLDELETEVERTEXARRAYSPROC>("glDeleteVertexArrays");
+  pingus_glDisableVertexAttribArray = load_proc<PFNGLDISABLEVERTEXATTRIBARRAYPROC>("glDisableVertexAttribArray");
+  pingus_glEnableVertexAttribArray = load_proc<PFNGLENABLEVERTEXATTRIBARRAYPROC>("glEnableVertexAttribArray");
+  pingus_glGenBuffers = load_proc<PFNGLGENBUFFERSPROC>("glGenBuffers");
+  pingus_glGenVertexArrays = load_proc<PFNGLGENVERTEXARRAYSPROC>("glGenVertexArrays");
+  pingus_glGetAttribLocation = load_proc<PFNGLGETATTRIBLOCATIONPROC>("glGetAttribLocation");
+  pingus_glGetProgramInfoLog = load_proc<PFNGLGETPROGRAMINFOLOGPROC>("glGetProgramInfoLog");
+  pingus_glGetProgramiv = load_proc<PFNGLGETPROGRAMIVPROC>("glGetProgramiv");
+  pingus_glGetShaderInfoLog = load_proc<PFNGLGETSHADERINFOLOGPROC>("glGetShaderInfoLog");
+  pingus_glGetShaderiv = load_proc<PFNGLGETSHADERIVPROC>("glGetShaderiv");
+  pingus_glGetUniformLocation = load_proc<PFNGLGETUNIFORMLOCATIONPROC>("glGetUniformLocation");
+  pingus_glLinkProgram = load_proc<PFNGLLINKPROGRAMPROC>("glLinkProgram");
+  pingus_glShaderSource = load_proc<PFNGLSHADERSOURCEPROC>("glShaderSource");
+  pingus_glUniform1i = load_proc<PFNGLUNIFORM1IPROC>("glUniform1i");
+  pingus_glUniform4fv = load_proc<PFNGLUNIFORM4FVPROC>("glUniform4fv");
+  pingus_glUniformMatrix4fv = load_proc<PFNGLUNIFORMMATRIX4FVPROC>("glUniformMatrix4fv");
+  pingus_glUseProgram = load_proc<PFNGLUSEPROGRAMPROC>("glUseProgram");
+  pingus_glVertexAttribPointer = load_proc<PFNGLVERTEXATTRIBPOINTERPROC>("glVertexAttribPointer");
 }
 
 } // namespace pingus
